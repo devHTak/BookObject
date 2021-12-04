d7f9782b4d71847c06200a9ba5273932a230d410
devHTak <qkrgusxkr1@gmail.com>
2021-12-04 11:40:26 +0900

    chap04. 리팩토링

--- src/main/java/com/example/object/chap04/entity/DiscountCondition.java
+++ src/main/java/com/example/object/chap04/entity/DiscountCondition.java
@@ -1,14 +1,11 @@
 package com.example.object.chap04.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
-@Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 public class DiscountCondition {
 
@@ -21,4 +18,29 @@ public class DiscountCondition {
     private LocalTime startTime;
 
     private LocalTime endTime;
+
+    public DiscountConditionType getType() {
+        return this.type;
+    }
+
+    /**
+     * 캡슐화 위반
+     * 파라미터를 통해 객체에 내부 상태를 노출시킨다.
+     */
+
+    public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+        if(this.type != DiscountConditionType.PERIOD)
+            throw new IllegalArgumentException();
+
+        return this.dayOfWeek.equals(dayOfWeek) &&
+                this.startTime.compareTo(time) <= 0 &&
+                this.endTime.compareTo(time) >= 0;
+    }
+
+    public boolean isDiscountable(int sequence) {
+        if(this.type != DiscountConditionType.SEQUENCE)
+            throw new IllegalArgumentException();
+
+        return this.sequence == sequence;
+    }
 }
--- src/main/java/com/example/object/chap04/entity/Movie.java
+++ src/main/java/com/example/object/chap04/entity/Movie.java
@@ -2,14 +2,12 @@ package com.example.object.chap04.entity;
 
 import com.example.object.chap02.entity.Money;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 public class Movie {
 
@@ -24,5 +22,56 @@ public class Movie {
     private MovieType movieType;
     private Money discountAmount;
     private double discountPercent;
+
+    public MovieType getMovieType() {
+        return this.movieType;
+    }
+
+    /**
+     * 캡슐화 문제점
+     * 1. calculate... 메서드 명을를 통해 할인 정책을 노출시키고 있다.
+     * 2. 만약 새로운 할인 정책이 추가된다면, 해당 메서드들에 의존하는 모든 클라이언트가 영향을 받을 것이다.
+     */
+    public Money calculateAmountDiscountFee() {
+        if(movieType != MovieType.AMOUNT_DISCOUNT)
+            throw new IllegalArgumentException();
+
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountFee() {
+        if(movieType != MovieType.PERCENT_DISCOUNT)
+            throw new IllegalArgumentException();
+
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calcluateNonDiscountFee() {
+        if(movieType != MovieType.NONE_DISCOUNT)
+            throw new IllegalArgumentException();
+
+        return fee;
+    }
+
+    /**
+     * 높은 결합도
+     * 캡슐화 위반으로 인해 Movie - DiscountPolicy 사이에 높은 결합도가 발생하였다.
+     * 즉, DiscountPolicy에서 변경이 발생하면 Movie에도 영향이 발생하여 수정이 필요하다
+     */
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for(DiscountCondition condition : discountConditions) {
+            if(condition.getType() == DiscountConditionType.PERIOD) {
+                if(condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                }
+            } else {
+                if(condition.isDiscountable(sequence)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 
 }
